5d86467738225a8f6599f40ef316cbad74a913e5
Dayanand Chinchure <102784873+dchinchure-cr@users.noreply.github.com>
2023-10-03 14:08:26 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 echo $1
-export GOOGLE_CLOUD_PROJECT=$1 
+echo $2
+export GOOGLE_CLOUD_PROJECT=$1
+export CHECKRED_SERVICE_ACCOUNT_EMAIL=$2
 gcloud services enable iamcredentials.googleapis.com --project $1
 sed -i "s/PROJECT_ID/$1/g" checkred.tf
+sed -i "s/CHECKRED_SERVICE_ACCOUNT_EMAIL/$2/g" checkred.tf
 terraform init
 terraform plan
 terraform apply -auto-approve
